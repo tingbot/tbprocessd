0aa2d17a6c586ec8f3782702cfaf1b4959402bb4
Joe Rickerby <joerick@mac.com>
2015-03-15 00:08:16 +0000

    Remove pygame code from test apps because they don't respond well to SIGTERM

--- testing/home.py
+++ testing/home.py
@@ -2,20 +2,6 @@
 from glob import glob
 import os
 import subprocess
-import pygame
-
-def screen():
-    pygame.init()
-    pygame.font.init()
-
-    surface = pygame.display.set_mode((320, 240))
-
-    font = pygame.font.Font(None, 32)
-    surface.fill((255,255,255))
-    surface.blit(font.render('Home screen', 1, (0,0,0)), (50, 50))
-    pygame.display.update()
-
-# screen()
 
 folder = os.path.dirname(__file__)
 
