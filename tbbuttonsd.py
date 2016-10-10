e0ab27800e353b804858a36c3db922f1f56541fa
Whistle Master <whistle_master@live.com>
2016-10-10 19:40:10 +0200

    Configurable combo from configuration file

--- tbbuttonsd.py
+++ tbbuttonsd.py
@@ -1,37 +1,65 @@
 from __future__ import print_function
 import subprocess, threading, signal
 from tingbot.platform_specific.tingbot import register_button_callback
+from Queue import Queue
+import json, os
 
-midleft_state = 'up'
-midright_state = 'up'
-
-home_event = threading.Event()
+button_states = ['up', 'up', 'up', 'up']
+actionsList = {}
+queue = Queue()
 
 def button_callback(button_index, state):
-    global midleft_state, midright_state
+    global button_states, queue
+    
+    button_states[button_index] = state
 
-    if button_index == 1:
-        midleft_state = state
-    elif button_index == 2:
-        midright_state = state
+    queue.put(tuple(button_states))
 
-    if midleft_state == 'down' and midright_state == 'down':
-        home_event.set()
-
-def respond_to_home_event():
+def process_combo_events():
+    global queue
+    
     while True:
-        home_event.wait()
-        print('Home combo detected')
-        subprocess.call(['tbopen', '/apps/home'])
-        home_event.clear()
+        combo = queue.get()
 
+        action = actionsList.get(combo)
+
+        if action:
+            print('Combo detected:')
+            print(combo)
+            print('Action:')
+            print(action)
+            subprocess.call(action)
+
+        queue.task_done()
+
+def load_json(filename):
+    try:
+        with open(filename, 'r') as fp:
+            result = json.load(fp)
+            return result
+    except ValueError:
+        raise ValueError('Failed to load %s because it\'s not a valid JSON file' % filename)
+    except IOError:
+        #either non-existent file or empty filename
+        return {}
+        
+def load_config():
+    if os.path.exists('tbbuttonsd.conf'):
+        config = load_json('tbbuttonsd.conf')
+    elif os.path.exists('/etc/tbbuttonsd.conf'):
+        config = load_json('/etc/tbbuttonsd.conf')
+
+    for action in config:
+        actionsList[tuple(action["combo"])] = action["command"]
+     
 def main():
+    load_config()
     register_button_callback(button_callback)
     
     # got to keep the main thread free to respond ot signals e.g. SIGTERM
     # Event.wait() will block signals until set so we push this to its own
     # thread.
-    respond_thread = threading.Thread(target=respond_to_home_event)
+    respond_thread = threading.Thread(target=process_combo_events)
     respond_thread.daemon = True
     respond_thread.start()
 
